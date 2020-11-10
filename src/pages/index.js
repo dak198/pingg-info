ec314bb467f36d2bb5775034f5d791ead3dc2870
dak198 <69974367+dak198@users.noreply.github.com>
2020-11-10 16:28:23 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,7 +73,7 @@ function Home() {
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                'button button--secondary',
+                'button button--info button--outline button--lg',
                 styles.getStarted,
               )}
               to={useBaseUrl('docs/')}>
