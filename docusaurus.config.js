8d2abe2e7a3cdf8d3e54b1ef3f0979917fde7230
Dominic <dak198@pitt.edu>
2020-11-10 07:49:31 +0000

    committing from my script

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ module.exports = {
           items: [
             {
               label: 'Product Overview',
-              to: 'docs/product-overview/',
+              to: 'docs/product-overview.md',
             },
           ],
         },
