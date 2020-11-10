bec508f7368d80f5cbffa0d7b4882f232ea4cd55
Dominic <dak198@pitt.edu>
2020-11-10 05:15:01 +0000

    committing from my script

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Docusaurus: ['product-overview', 'getting-started', 'modes-of-operation'],
     Features: ['mdx'],
   },
 };
