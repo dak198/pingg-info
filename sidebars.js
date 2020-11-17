7c2f2c70404e7b3d4f0751854ba0781302cb3a1d
Joe Paul <jjp106@pitt.edu>
2020-11-16 21:41:39 -0500

    Deleted sample runs, no use on a help site

--- sidebars.js
+++ sidebars.js
@@ -1,25 +1,10 @@
 module.exports = {
-  someSidebar: {
-    "Help Site": [
-      'product-overview',
-      {
-        type: 'category',
-        label: 'Getting Started',
-        items: [
-          'login',
-          {
-            type: 'category',
-            label: 'Sample Runs',
-            items: ['post-sample', 'profile-sample',],
-          },
-        ],
-      },
-      {
-        type: 'category',
-        label: 'Modes of Operation',
-        items: ['profile-customization', 'search', 'create-post', 'report-issue'],
-      },
-      'tutorial',
-    ],
-  },
+  someSidebar: [
+    'product-overview',
+    'login',
+    'profile-customization', 
+    'search', 
+    'create-post', 
+    'report-issue',
+  ],
 };
